ec54fb619f54192450e936ad8f24a56c290b681e
Stefan_Karlsson <gittest@k-son.se>
2020-12-03 21:17:24 +0100

    03

--- 03/aoc2020_03.py
+++ 03/aoc2020_03.py
@@ -18,5 +18,36 @@ def one(filename):
                 right = (right + 3) % len(inputline)
     return treecount
 
+
+def two(filename):
+    with open(filename) as f:
+        runs = ((1,1),(3,1),(5,1),(7,1),(1,2))
+        pos = [0]*len(runs)
+        treecounts = [0]*len(runs)
+        down = 0
+        while True:
+            inputline = f.readline().strip("\n")
+            if not inputline: 
+                break
+            else:
+                for (j,run) in enumerate(runs):
+                    if not down % run[1]:
+                        if inputline[pos[j]] == '#':
+                            treecounts[j] += 1
+                            #print( inputline[:pos[j]]+"X"+inputline[pos[j]+1:])
+                        else:
+                            pass #print( inputline[:pos[j]]+"O"+inputline[pos[j]+1:])
+                        pos[j] = (pos[j] + run[0]) % len(inputline)
+                    else: 
+                        pass #print(inputline)
+                    
+                down += 1
+        return treecounts
+
 print( one("input.txt"))
+treecounts = two("input.txt")
+p=1
+for c in treecounts:
+    p *= c
+print(p, treecounts)
 
