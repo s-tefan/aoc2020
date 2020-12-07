ff5c3d6e527f2218e1a2dc6d0085abc5568c6c8f
Stefan_Karlsson <gittest@k-son.se>
2020-12-07 21:44:20 +0100

    07 refactored

--- 07/aoc2020_07.py
+++ 07/aoc2020_07.py
@@ -4,10 +4,7 @@ def read(f):
         a, b = line.strip().split(" contain ")
         a_cattr, a_color, _ = a.split(" ")
         b_list = b.strip(".").split(", ")
-        if b_list[0][:3] == "no ":
-            contain_dict[(a_cattr, a_color)] = tuple()
-        else:                 
-            contain_dict[(a_cattr, a_color)] = tuple(map(fix, b_list))
+        contain_dict[(a_cattr, a_color)] = tuple() if b_list[0][:3] == "no " else tuple(map(fix, b_list))   
     return contain_dict
 
 def fix(bagitem):
@@ -21,20 +18,15 @@ def find_containers(bag, contain_dict):
     return set(k for k in contain_dict if bag in contain_dict[k])
 
 def find_containers_transitively(bags, contain_dict):
-    new = set()
-    for bag in bags:
-        new = new.union(find_containers(bag, contain_dict))
-    if new.issubset(bags):
-        return bags
-    else:
-        return find_containers_transitively(bags.union(new), contain_dict)
+        new = set.union(*[find_containers(bag, contain_dict) for bag in bags])
+        return bags if new.issubset(bags) else find_containers_transitively(bags.union(new), contain_dict)
 
 def count_bags(bag, contain_dict):
     return sum(bag[1]*(1+count_bags(bag,contain_dict)) for bag in contain_dict[bag[0]])
 
 with open("input.txt") as f:
     bag_dict = read(f)
-    a_dict = simplify(bag_dict)
+a_dict = simplify(bag_dict)
 my_bag = ("shiny", "gold")
 print("Part one:",len(find_containers_transitively(find_containers(my_bag, a_dict), a_dict)))
 print("Part two:", count_bags((my_bag, 1), bag_dict))
