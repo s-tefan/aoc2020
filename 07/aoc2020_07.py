41fe430142c53d50826268c3ed55600fdcf6e9a2
Stefan_Karlsson <gittest@k-son.se>
2020-12-08 01:46:39 +0100

    documented

--- 07/aoc2020_07.py
+++ 07/aoc2020_07.py
@@ -1,30 +1,41 @@
-def read(f):
+""" Advent of Code 2020, day 7."""
+__author__ = "Stefan Karlsson"
+__date__ = "2020-12-07"
+
+def read(f): 
+    """Read lines from file and make a corresponding dict."""
     contain_dict = {}
     for line in f:
-        a, b = line.strip().split(" contain ")
+        a, b = line.strip().split(" contain ") 
         a_cattr, a_color, _ = a.split(" ")
         b_list = b.strip(".").split(", ")
         contain_dict[(a_cattr, a_color)] = tuple() if b_list[0][:3] == "no " else tuple(map(fix, b_list))   
     return contain_dict
 
-def fix(bagitem):
+def fix(bagitem): 
+    """Make a nice dict for the pair of a color and number."""
     n, cattr, color, _ = bagitem.split(" ")
     return {'c': (cattr,color), 'n': int(n)}
 
-def simplify(contain_dict):
+def simplify(contain_dict): 
+    """Get rid of the numbers."""
     return {k:tuple(map(lambda a : a['c'], contain_dict[k])) for k in contain_dict}
 
-def find_containers(bag, contain_dict):
+def find_containers(bag, contain_dict): 
+    """Find all bags that must contain a given bag."""
     return set(k for k in contain_dict if bag in contain_dict[k])
 
-def find_containers_transitively(bags, contain_dict):
-        new = set.union(*(find_containers(bag, contain_dict) for bag in bags))
-        return bags if new.issubset(bags) else find_containers_transitively(bags.union(new), contain_dict)
+def find_containers_transitively(bags, contain_dict): 
+    """Recursively take the transitive closure of containment."""
+    new = set.union(*(find_containers(bag, contain_dict) for bag in bags))
+    return bags if new.issubset(bags) else find_containers_transitively(bags.union(new), contain_dict)
 
-def count_bags(bag, contain_dict):
+def count_bags(bag, contain_dict): 
+    """Recursively count the total number of bags in a given bag (including the bag itself)."""
     return sum(bag['n']*(1+count_bags(bag,contain_dict)) for bag in contain_dict[bag['c']])
 
-with open("input.txt") as f:
+"""Start the calculation!"""
+with open("input.txt") as f: 
     bag_dict = read(f)
 a_dict = simplify(bag_dict)
 my_bag = ("shiny", "gold")
