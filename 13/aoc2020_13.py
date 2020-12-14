17536f0ce452a97599fd66a1e5a6a3cbdf6f7c35
Stefan_Karlsson <gittest@k-son.se>
2020-12-14 14:17:08 +0100

    Trying an alternative sieve method for CRT

--- 13/aoc2020_13.py
+++ 13/aoc2020_13.py
@@ -22,26 +22,45 @@ def partone(filename):
     return (minimal['id']*minimal['t'])
 
 def parttwo(filename):
+    '''Using the chinese remainder theorem with euclidian algorithm.'''
     with open(filename) as f:
         departure = int(f.readline().strip())
         strlist = map(lambda x: x.strip(), f.readline().split(','))
-        buslist = list(map(lambda x: (x[0], int(x[1])), filter(lambda x: x[1]!='x', enumerate(strlist))))
-        # Check coprimality
-        mlist = [bus[1] for bus in buslist]
-        for i, a in enumerate(mlist):
-            for b in mlist[:i]:
-                d, _, _ = xeuclid(a,b)
-                if d != 1: raise Exception("Not coprime, solution not implemented.") 
-        # Chinese remainder solving, for coprime moduli
-        N = reduce(lambda x1, x2: x1*x2[1], buslist, 1) # Product of all moduli (bus periods)
-        x = 0 # Initialize common solution
-        for k, bus in enumerate(buslist):
-            a, m = bus # a is the departure offset, m the periodicity, for each bus line
-            _, x1, _ = xeuclid(N//m, m) # Solve x1*N//m % m == 1
-            x += x1*(-a)*N//m # Add solution to x*N//m % m == -a to common solution
-            x = x % N # reduce partial solution modulo n
-        if [None for bus in buslist if (x+bus[0]) % bus[1] != 0]: # Check. Empty list if correct solution. 
-            raise Exception("Not a correct solution!")
-        return(x)
-        
+    buslist = list(map(lambda x: (x[0], int(x[1])), filter(lambda x: x[1]!='x', enumerate(strlist))))
+    # Check coprimality
+    mlist = [bus[1] for bus in buslist]
+    for i, a in enumerate(mlist):
+        for b in mlist[:i]:
+            d, _, _ = xeuclid(a,b)
+            if d != 1: raise Exception("{},{} not coprime, solution not implemented.".format(a,b)) 
+    # Chinese remainder solving, for coprime moduli
+    N = reduce(lambda x1, x2: x1*x2[1], buslist, 1) # Product of all moduli (bus periods)
+    x = 0 # Initialize common solution
+    for k, bus in enumerate(buslist):
+        a, m = bus # a is the departure offset, m the periodicity, for each bus line
+        _, x1, _ = xeuclid(N//m, m) # Solve x1*N//m % m == 1
+        x += x1*(-a)*N//m # Add solution to x*N//m % m == -a to common solution
+        x = x % N # reduce partial solution modulo n
+    if [None for bus in buslist if (x+bus[0]) % bus[1] != 0]: # Check. Empty list if correct solution. 
+        raise Exception("Not a correct solution!")
+    return(x)
+
+def parttwo_sieve(filename):
+    '''Does not work as expected.
+    Works on test case if making list of sieve_sequance in each step.
+    Out of memory for problem.
+    Does not work on test case when usig lazy filter. Why?
+    '''
+    with open(filename) as f:
+        departure = int(f.readline().strip())
+        strlist = map(lambda x: x.strip(), f.readline().split(','))
+    buslist = list(map(lambda x: (x[0], int(x[1])), filter(lambda x: x[1]!='x', enumerate(strlist))))
+    #print(buslist)
+    N = reduce(lambda x1, x2: x1*x2[1], buslist, 1) # Product of all moduli (bus periods)
+    sieve_sequence = range(0, N, buslist[0][1])
+    for bus in buslist[1:]:
+        sieve_sequence = filter(lambda x: (x + bus[0]) % bus[1] == 0, sieve_sequence)
+    return list(sieve_sequence)[:5]
+
 print(partone("input.txt"), parttwo("input.txt"))
+#print(parttwo_sieve("test.txt"))
