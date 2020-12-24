b9ec5ac486e5c39078cccac1041546cef0d44066
Stefan_Karlsson <gittest@k-son.se>
2020-12-24 10:42:32 +0100

    24 ok

--- 24/aoc2020_24.py
+++ 24/aoc2020_24.py
@@ -19,7 +19,24 @@ class Hextile:
             self.coords = self.coords[0] + d[0], self.coords[1] + d[1]
         Hextile.blacklist = Hextile.blacklist.symmetric_difference({
                                                                    self.coords})
+    @classmethod
+    def neighbours(cls):
+        neighbourdict = {}
+        for c in cls.blacklist:
+            for d in cls.dircoords.values():
+                e = (c[0]+d[0],c[1]+d[1])
+                if e in neighbourdict.keys():
+                    neighbourdict[e] += 1
+                else:
+                    neighbourdict[e] = 1
+        blackflip = cls.blacklist.difference(neighbourdict.keys()).\
+            union({a for (a,b) in neighbourdict.items() if (b > 2)})
+        whiteflip =  {a for (a,b) in neighbourdict.items() if b == 2 and a not in cls.blacklist}
+        cls.blacklist = cls.blacklist.difference(blackflip).union(whiteflip)
 
+    @classmethod
+    def reset(cls):
+        cls.blacklist = set()
 
 def partone(filename):
     with open(filename) as f:
@@ -27,5 +44,13 @@ def partone(filename):
             Hextile().moveflip(line.strip('\n'))
     return len(Hextile.blacklist)
 
+def parttwo(filename, verbose = False):
+    Hextile.reset()
+    partone(filename)
+    for k in range(100):
+        Hextile.neighbours()
+        if verbose: print('Day {}: {}', k+1, len(Hextile.blacklist) )
+    return len(Hextile.blacklist)
 
 print(partone("input.txt"))
+print(parttwo("input.txt"))
