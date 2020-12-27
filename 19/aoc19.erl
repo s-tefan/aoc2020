cf814156aace02a68e5ac969ef05d5d8c7daa3ab
Stefan_Karlsson <gittest@k-son.se>
2020-12-27 12:54:15 +0100

    19 ok erlang

--- 19/aoc19.erl
+++ 19/aoc19.erl
@@ -1,41 +1,44 @@
--module(aoc19).
--export([partone/1, partone/2]).
+-module(aoc19v2).
+-export([check/3, read/1, partone/1, test/2, parttwo/1]).
 
-partone(Filename) -> partone(Filename, false).
-
-partone(Filename, Verbose) ->
+partone(Filename) ->
     {Rules, Messages} = read(Filename),
-    nr(evaluate(Rules, Messages, Verbose)).
+    length(lists:filter(fun({B,R}) -> B and (R=="") end, 
+        lists:map(fun(M) -> aoc19v2:check(M, dict:fetch(0, Rules), Rules) end, 
+            Messages))).
 
-evaluate(_, [], _) ->
-    [];
-evaluate(Rules, [H| T], Verbose) ->
-    {B, _} = check(Rules, {start, 0}, H),
-    case Verbose of 
-        V when V == true; V == verbose ->
-            io:format("~w ~s~n", [B, H]); 
-        _ -> ok 
-    end,
-    [B| evaluate(Rules, T, Verbose)].
+parttwo(Filename) ->
+    {Rules, Messages} = read(Filename),
+    length(lists:filter(fun(M) -> parttwocheck(M, Rules) end, Messages)).
 
-nr([]) -> 0;
-nr([B| T]) ->
-    case B of
-        true -> 1 + nr(T);
-        false -> nr(T)
+parttwocheck(S,Rules) ->
+    {N42, Rest1} = count(42,S,Rules,0),
+    {N31, Rest2} = count(31,Rest1,Rules,0),
+    if 
+        N42 >= 2, N31 >=1, N42 > N31, Rest2 == "" ->
+            true;
+        true ->
+            false
     end.
 
-% Functions for reading a file into a rule structure,
-% a dict of integer keyed rules, where a rule is a tuple of rules
-% {char, char()} | {orlist, [{andlist, [integer()]}] 
-% 
+count(Rnr,S,Rules,N) ->
+    case check(S, dict:fetch(Rnr, Rules), Rules) of
+        {true, Rest} ->
+            count(Rnr,Rest,Rules,N+1);
+        {false, _} ->
+            {N, S}
+    end.
+
+test(Filename, Start) ->
+    {Rules, Messages} = read(Filename),
+        lists:map(fun(M) -> aoc19v2:check(M, dict:fetch(Start, Rules), Rules) end, 
+            Messages).
 
 read(Filename) ->
     {ok, File} = file:open(Filename,[read]),
     Rules = readlines(File, rules, dict:new()),
     Messages = readlines(File, messages, []),
     {Rules, Messages}.
-    
 
 readlines(File, rules, Acc) ->
     % Acc är en dict
@@ -47,7 +50,7 @@ readlines(File, rules, Acc) ->
             Acc;
         {ok, Line} ->
             {N, V} = readrule(Line),
-            readlines(File, rules, dict:append(N, V, Acc));
+            readlines(File, rules, dict:store(N, V, Acc));
         {error, read_line} -> 
             error
     end;
@@ -72,19 +75,20 @@ readrule(Line) ->
         [$", C|  _] -> 
             {N, {char, C}};
         _ -> 
-            {N, {orlist, orsplit(S)}}
+            {N, orsplit(S)}
     end.
 
 orsplit(Rulestr) ->
-    case string:split(string:trim(Rulestr), "|") of
+    case
+         string:split(string:trim(Rulestr), "|") of
         [] -> 
             [];
-        [S] -> [{andlist,andsplit(S)}];
+        [S] -> [andsplit(S)];
         [H, T] ->
-            [{andlist, andsplit(H)}| orsplit(T)]
+            [andsplit(H)| orsplit(T)]
     end.
 
-andsplit(Str) ->
+andsplit(Str) ->    
     case string:to_integer(string:trim(Str)) of
         {N, []} -> 
             [N];
@@ -92,38 +96,51 @@ andsplit(Str) ->
             [N| andsplit(T)]
     end.
 
+check("", _, _) ->
+    {false, ""};
+check(S, Rule, Rules) ->
+    case Rule of
+        {char, C} ->
+            case C == hd(S) of
+                true ->
+                    {true, tl(S)};
+                false ->
+                    {false, S}
+            end;
+        [] ->
+            {false, S};
+        [[]] ->
+            {true, S};
+        [OH| OT] ->
+            case andcheck(S, OH, S, Rules) of
+                {true, Rest} ->
+                    {true, Rest};
+                {false, _} ->
+                    check(S, OT, Rules)
+            end
+    end.
 
-% Functions for checking if a string follows a set of rules,
-% a dict of integer keyed rules, where a rule is a tuple of rules
-% {char, char()} | {orlist, [{andlist, [integer()]}] 
-% 
-
-% check(Rules, Rule, Mess) -> {Result :: boolean(), MessRest :: string()}
-
-check(Rules, {start, N}, M) ->
-    {B, M1} = check(Rules, hd(dict:fetch(N, Rules)), M),
-    {B and (M1 == ""), M1};
-check(_, {andlist, []}, M) -> 
-    {true, M};
-check(Rules, {andlist, [AH| AT]}, M) ->
-    {B1, M1} = check(Rules, hd(dict:fetch(AH, Rules)), M),
-    case B1 of
-        true -> 
-            check(Rules, {andlist, AT}, M1);
+andcheck([], [], _, _) ->
+    {true, ""};
+andcheck([], [_], _, _) ->
+    {false, ""};
+andcheck(S, [], _, _) ->
+    {true, S};
+andcheck(S, [Rnr| RT], Smem, Rules) ->
+    {Match, Rest} = check(S, dict:fetch(Rnr, Rules), Rules),
+    case Match of
+        true ->
+            andcheck(Rest, RT, Smem, Rules);
         false ->
-            {false, M1}
-    end;
-check(_, {orlist, []}, M) -> 
-    {false, M};
-check(Rules, {orlist, [OH| OT]}, M) ->
-    {B, M1} = check(Rules, OH, M),
-    case B of
-        true -> {true, M1};
-        false -> check(Rules, {orlist, OT}, M)
-    end;
-check(_, {char, _}, []) ->
-    {false, []};
-check(_, {char, C}, [MH| MT]) ->
-    {C == MH, MT}.
+            {false, Smem}
+    end.
+
+
+
+
+            
+
+
+
 
 
